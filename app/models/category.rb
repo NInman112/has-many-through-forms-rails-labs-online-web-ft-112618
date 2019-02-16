a7b346ac088d54f45d663f892ac02d24dcdcd65d
Nick Inman <nick@theoatmeal.com>
2019-02-16 05:31:13 +0000

    Done.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ActiveRecord::Base
   has_many :post_categories
   has_many :posts, through: :post_categories
+
+  def self.category_join(params)
+    params.map {|category| category.name }.join(", ")
+  end
 end
