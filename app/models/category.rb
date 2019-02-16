8f32b6ae1a5cbae53c500489b445c882e74d0ab2
Nick Inman <nick@theoatmeal.com>
2019-02-16 05:25:45 +0000

    Automatically backed up by Learn

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,6 @@ class Category < ActiveRecord::Base
   has_many :posts, through: :post_categories
 
   def self.category_join(params)
-    params.map {|category| category.name }.join(", ")}
-    binding.pry
+    params.map.tap {|category| category.name }.join(", ")
   end
 end
