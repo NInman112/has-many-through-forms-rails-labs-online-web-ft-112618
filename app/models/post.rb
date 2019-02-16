632788bd17833cfad61f48234344e84b2b9a033b
Nick Inman <nick@theoatmeal.com>
2019-02-16 05:02:49 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,16 +5,4 @@ class Post < ActiveRecord::Base
   has_many :users, through: :comments
   accepts_nested_attributes_for :categories
 
-
-  # def categories_attributes=(categories_hashes)
-  #   categories_hashes.each do |i, category_attributes|
-  #     if category_attributes[:name].present?
-  #       category = Category.find_or_create_by(name: category_attributes[:name])
-  #       if self.categories.exclude?(category)
-  #         self.post_categories.build(:category => category)
-  #       end
-  #     end
-  #   end
-  # end
-
 end
