8027dbaaf56a0fb500c89bb29888fe72b66276d7
Nick Inman <nick@theoatmeal.com>
2019-02-16 04:51:36 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,13 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-  accepts_nested_attributes_for :user
-  
+  accepts_nested_attributes_for :user, reject_if: :reject_posts
+
+  def reject_posts(attributes)
+    binding.pry
+    attributes['title'].blank?
+  end
+
   def user_attributes=(user_attributes)
     user = User.find_or_create_by(user_attributes)
     self.user = user
